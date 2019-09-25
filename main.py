1db44cd14a1c35614e22919e7b39d3423a4a13dc
Xiao Qinfeng <qfxiao@bjtu.edu.cn>
2019-09-24 23:27:48 -0400

    Add model saving

--- main.py
+++ main.py
@@ -24,17 +24,16 @@ from util import WeightedCrossEntropy, FocalLossMultiClass
 from config import *
 
 
-# def set_gpu(num_gpu=1, verbose=True):
-    # os.system('nvidia-smi -q -d Memory | grep -A4 GPU | grep Free > tmp')
-    # gpu_memory = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
-    # gpu_ids = np.argsort(-1*np.array(gpu_memory))
-    # os.system('rm tmp')
-    # assert(num_gpu <= len(gpu_ids))
-    # os.environ['CUDA_VISIBLE_DEVICES']=','.join(map(str, gpu_ids[:num_gpu]))
-    # if verbose:
-    #     print('Current GPU [%s], free memory: [%s] MB'%(os.environ['CUDA_VISIBLE_DEVICES'], ','.join(map(str, np.array(gpu_memory)[gpu_ids[:num_gpu]]))))
-
 def train(epoch, model, optimizer, criterion, train_loader, val_loader=None):
+    """
+    Train the network for an epoch. Executing model.train() before invoking this function is recommended.
+    @param epoch: The current epoch
+    @param model: The current model
+    @param optimizer: The current optimizer
+    @param criterion: The loss function
+    @param train_loader: Training dataset loader
+    @param val_loader: Validation dataset loader
+    """
     total_train_loss = []
     total_val_loss = []
     with tqdm(train_loader, desc='Epoch: [%d/%d]'%(epoch+1, EPOCHS)) as loader:
@@ -64,10 +63,54 @@ def train(epoch, model, optimizer, criterion, train_loader, val_loader=None):
                         loader.set_postfix({'train_loss': np.nan if len(total_train_loss)==0 else np.mean(total_train_loss), 'val_loss': np.nan if len(total_val_loss)==0 else np.mean(total_val_loss)})
 
 
-def test(model, test_loader):
+def evaluate(model, data_loader):
+    """
+    Evaluate the model performance. Executing model.eval() before invoking this function is recommended.
+    """
+    results = []
+    target = []
     with torch.no_grad():
-        output = np.array([model(x.cuda()).detach().cpu().numpy() for x in test_loader])
-    output = np.squeeze(output, axis=1)
+        for x, y in tqdm(data_loader):
+            x = x.cuda()
+            output = model(x).detach().cpu().numpy()
+            results.append(output)
+            target.append(y.numpy())
+    result_expect_last = np.array(results[:-1]).reshape(-1, NUM_CLASSES)
+    result_last = np.array(results[-1]).reshape(-1, NUM_CLASSES)
+    results = np.concatenate((result_expect_last, result_last), axis=0)
+
+    target_expect_last = np.array(target[:-1]).reshape(-1, NUM_CLASSES)
+    target_last = np.array(target[-1]).reshape(-1, NUM_CLASSES)
+    target = np.concatenate((target_expect_last, target_last), axis=0).astype(np.int)
+
+    result = np.zeros_like(results).astype(np.int)
+    result[results>=0.5] = 1
+
+    precision_micro = precision_score(result, target, average='micro')
+    precision_macro = precision_score(result, target, average='macro')
+    recall_micro = recall_score(result, target, average='micro')
+    recall_macro = recall_score(result, target, average='macro')
+    f1_micro = f1_score(result, target, average='micro')
+    f1_macro = f1_score(result, target, average='macro')
+
+    with open('output/result.txt', 'w', encoding='utf8') as f:
+        f.write('Presion Micro: %f\n'%precision_micro)
+        f.write('Presion Macro: %f\n'%precision_macro)
+        f.write('Recall Micro: %f\n'%recall_micro)
+        f.write('Recall Macro: %f\n'%recall_macro)
+        f.write('F1 Micro: %f\n'%f1_micro)
+        f.write('F1 Macro: %f\n'%f1_macro)
+
+
+def test(model, test_loader):
+    """
+    Output results. Executing model.eval() before invoking this function is recommended.
+    """
+    with torch.no_grad():
+        output = [model(x.cuda()).detach().cpu().numpy() for x in tqdm(test_loader)]
+        output_expect_last = np.array(output[:-1]).reshape(-1, NUM_CLASSES)
+        output_last = np.array(output[-1]).reshape(-1, NUM_CLASSES)
+        output = np.concatenate((output_expect_last, output_last), axis=0)
     result = np.zeros_like(output).astype(np.int)
     result[output>=0.5] = 1
 
@@ -77,7 +120,7 @@ def test(model, test_loader):
     with open('data/test/hf_round1_subA.txt', 'r', encoding='utf8') as f:
         test_contents = f.readlines()
 
-    with open('testA.txt', 'w', encoding='utf8') as f:
+    with open('output/testA.txt', 'w', encoding='utf8') as f:
         for i, line in tqdm(enumerate(test_contents)):
             line = line[:-1]
             for j in range(result.shape[1]):
@@ -87,6 +130,26 @@ def test(model, test_loader):
             f.write(line)
     
     return result
+
+
+def save_model(model, path):
+    torch.save(model, path)
+
+
+def save_parameters(model, path):
+    torch.save(model.state_dict(), path)
+
+
+def load_model(path):
+    model = torch.load(path)
+    
+    return model
+
+
+def load_parameters(model, path):
+    model.load_state_dict(torch.load(path))
+
+    return model
 
 
 if __name__ == '__main__':
@@ -105,7 +168,7 @@ if __name__ == '__main__':
         print('Current GPU [%s], free memory: [%s] MB'%(os.environ['CUDA_VISIBLE_DEVICES'], ','.join(map(str, np.array(gpu_memory)[gpu_ids[:num_gpu]]))))
 
     frame = inspect.currentframe() # define a frame to track
-    gpu_tracker = MemTracker(frame) # define a GPU tracker
+    gpu_tracker = MemTracker(frame, path='output/') # define a GPU tracker
     gpu_tracker.track() # run function between the code line where uses GPU
 
     if MULTI_GPU:
@@ -126,7 +189,7 @@ if __name__ == '__main__':
     train_size = int(len(dataset)*TRAIN_SPLIT)
     val_size = len(dataset) - train_size
     train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
     val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
 
     # Training
@@ -136,9 +199,16 @@ if __name__ == '__main__':
         train(epoch, model, optimizer, criterion, train_loader, val_loader)
     gpu_tracker.track() # run function between the code line where uses GPU
 
+    if SAVE_MODEL:
+        save_model(model, path='output/resnet_kernel3_epoch20.pkl')
+
+    # Evaluating
+    model.eval()
+    evaluate(model, val_loader)
+
     # Testing
     test_dataset = ECGData(phase='test')
-    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS, drop_last=False)
     model.eval()
     result = test(model, test_loader)
 
